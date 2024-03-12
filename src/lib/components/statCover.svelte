699225e6554b794a40e3182e3dbb096bac7f9654
Wojciech Misiak <wojmis3@gmail.com>
2024-03-12 01:28:20 +0100

    Make height change for stations

--- src/lib/components/statCover.svelte
+++ src/lib/components/statCover.svelte
@@ -2,21 +2,22 @@
     import { polarToCartesian, rotateY, rotateZ, rotateX, cartesianToPolar, polarToEarth, baseTraj } from '../util'
     export let station: {id: number, theta: number, phi: number};
     export let minuteDelay: number = 0
-    export let stationH: number = 380
     export let stationN: number = 16
     let stheta:any, sphi:any, zeroCircle:any, baseCircle:any, delayCircle:any, inclination:any
 
     $: stationDelay = stationN*2*Math.PI*minuteDelay/60/24
     $: inclination = 2*Math.PI/stationN
     const R = 6371
-    let alpha = Math.acos(R/(R+stationH))
+    let alpha:any, stationH;
     //THESE ARE PROPER POLARS, NOT EARTH
 
     let points = 50
+    let stationHeights = [5165, 4182, 3385, 2722, 2162, 1681, 1262, 894, 567, 274]
     $:{
         stheta = Math.PI*station.theta/180
         sphi = Math.PI*station.phi/180
-
+        stationH = stationHeights[stationN-7]
+        alpha = Math.acos(R/(R+stationH))
     zeroCircle = [...Array(points).keys()].map((i:number)=>{
         let theta = alpha
         let phi = 2*Math.PI*i/points
